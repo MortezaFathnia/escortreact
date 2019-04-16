0ddddf3857c36abe6ff58c34855e3464d173e865
MortezaFathnia <Morfathniamorteza@gmail.com>
2019-04-16 16:39:03 +0430

    contextapi is developing

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import TextInputGroup from '../layout/TextInputGroup';
 import axios from 'axios';
 import classes from './Login.module.css';
+import { Consumer } from '../context';
 
 class Login extends Component {
   state = { mobile: '' };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-  onSubmit = async e => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
 
     const { mobile } = this.state;
@@ -17,6 +18,7 @@ class Login extends Component {
       mobile: mobile
     });
     if (res.data.status === 'success') {
+      dispatch({ type: 'LOGIN', payload: mobile });
       this.props.history.push('/confirm');
     } else {
       console.log(res.data.errorMassage);
@@ -26,24 +28,31 @@ class Login extends Component {
   render() {
     const { mobile } = this.state;
     return (
-      <div className={classes.loginContainer}>
-        <form onSubmit={this.onSubmit}>
-          <TextInputGroup
-            label="لطفا شماره موبایل خود را وارد کنید"
-            name="mobile"
-            placeholder="09*********"
-            value={mobile}
-            type="text"
-            onChange={this.onChange}
-          />
-          <input
-            type="submit"
-            value="دریافت کد تایید"
-            className="btn btn-block"
-          />
-        </form>
-        <div className={classes.loginBg} />
-      </div>
+      <consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className={classes.loginContainer}>
+              <form onSubmit={this.onSubmit.bind(this, dispatch)}>
+                <TextInputGroup
+                  label="لطفا شماره موبایل خود را وارد کنید"
+                  name="mobile"
+                  placeholder="09*********"
+                  value={mobile}
+                  type="text"
+                  onChange={this.onChange}
+                />
+                <input
+                  type="submit"
+                  value="دریافت کد تایید"
+                  className="btn btn-block"
+                />
+              </form>
+              <div className={classes.loginBg} />
+            </div>
+          );
+        }}
+      </consumer>
     );
   }
 }
--- src/context.js
+++ src/context.js
@@ -2,9 +2,23 @@ import React, { Component } from 'react';
 
 const Context = React.createContext();
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        
+      };
+    default:
+      return state;
+  }
+};
+
 export class Provider extends Component {
   state = {
-    authenticated: false
+    authenticated: false,
+    number: '',
+    os: '',
+    dispatch: action => this.setState(state => reducer(state, action))
   };
 
   render() {
